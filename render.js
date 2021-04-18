3d0fdb2b58ec2f53ff8196be7e7f4b7badd404e5
dark-0ne <17182126+dark-0ne@users.noreply.github.com>
2021-04-19 04:08:23 +0430

    Add border, margin, padding, background and font color

--- render.js
+++ render.js
@@ -37,47 +37,64 @@ client.connect(err => {
                     const row = document.createElement("div")
                     row.classList.add("row")
                     row.classList.add("align-items-center")
+                    row.classList.add("border")
+                    row.classList.add("rounded")
+                    row.classList.add("bg-light")
+                    row.classList.add("py-1")
+                    row.classList.add("my-1")
+                    row.classList.add("character-row")
 
                     const player_counter = document.createElement("div")
                     player_counter.classList.add("col-1")
                     player_counter.textContent = counter++
+                    player_counter.classList.add("player-counter")
                     row.appendChild(player_counter)
 
                     const class_div = document.createElement("div")
                     class_div.classList.add("col-1")
                     const class_icon = document.createElement("img")
-                    class_icon.setAttribute("width",40)
-                    class_icon.setAttribute("height",40)
+                    class_icon.setAttribute("width", 40)
+                    class_icon.setAttribute("height", 40)
                     switch (player.character_class) {
                         case "Warrior":
                             class_icon.setAttribute("src", "media/images/01-Warrior.png")
+                            row.classList.add("warrior-row")
                             break;
                         case "Paladin":
                             class_icon.setAttribute("src", "media/images/02-Paladin.png")
+                            row.classList.add("paladin-row")
                             break;
                         case "Death Knight":
                             class_icon.setAttribute("src", "media/images/03-DeathKnight.png")
+                            row.classList.add("death-knight-row")
                             break;
                         case "Hunter":
                             class_icon.setAttribute("src", "media/images/04-Hunter.png")
+                            row.classList.add("hunter-row")
                             break;
                         case "Shaman":
                             class_icon.setAttribute("src", "media/images/05-Shaman.png")
+                            row.classList.add("shaman-row")
                             break;
                         case "Rogue":
                             class_icon.setAttribute("src", "media/images/06-Rogue.png")
+                            row.classList.add("rogue-row")
                             break;
                         case "Druid":
                             class_icon.setAttribute("src", "media/images/07-Druid.png")
+                            row.classList.add("druid-row")
                             break;
                         case "Mage":
                             class_icon.setAttribute("src", "media/images/08-Mage.png")
+                            row.classList.add("mage-row")
                             break;
                         case "Warlock":
                             class_icon.setAttribute("src", "media/images/09-Warlock.png")
+                            row.classList.add("warlock-row")
                             break;
                         case "Priest":
                             class_icon.setAttribute("src", "media/images/10-Priest.png")
+                            row.classList.add("priest-row")
                             break;
                     }
                     class_div.appendChild(class_icon)
@@ -103,6 +120,7 @@ client.connect(err => {
                     const armory_button = document.createElement("button")
                     armory_button.classList.add("btn")
                     armory_button.classList.add("btn-primary")
+                    armory_button.classList.add("btn-sm")
                     armory_button.textContent = "Armory"
                     armory_button.setAttribute('onclick', "ipcRenderer.invoke('open-armory','" + player.armory_link + "')")
                     armory_div.appendChild(armory_button)
